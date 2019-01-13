181ec9f35c7c4dafa11c12e421cac24fb7e25fe3
mrs.nguyen2503@gmail.com <>
2019-01-13 13:52:31 +0700

    styling for component

--- src/App.js
+++ src/App.js
@@ -44,17 +44,26 @@ class App extends Component {
   };
 
   render() {
+    const buttonStyle = {
+      padding: "10px 15px",
+      backgroundColor: "#ccc",
+      borderRadius: "4px"
+    };
+
     let jsx = (
       <div className="App">
         <h1>Hello Reactjs 2</h1>
-        <button onClick={this.switchNameHandler.bind(this, "Dung", "18")}>Switch Name</button>
+        <button style={buttonStyle} onClick={this.switchNameHandler.bind(this, "Dung", "18")}>Switch Name</button>
         <Person
           name={this.state.person[0].name} 
           age={this.state.person[0].age}
           click={this.switchNameHandler.bind(this, "Hấu", "26")}
           changeName={this.nameChangeHandler}>
         </Person>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My favorite is Hau</Person>
+        <Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age}
+          changeName={this.nameChangeHandler}>My favorite is Hau</Person>
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import "./Person.css";
 
 var person = (pros) => {
     return (
-        <div>
+        <div className="person">
             <p onClick={pros.click}>I'm {pros.name} and i'm {pros.age} year(s) old</p>
             <p>{pros.children}</p>
             <input onChange={pros.changeName} value={pros.name}/>
